aa529e9ae79dbb5eae8d5441043cb6507b9e1e24
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-04-05 01:04:11 +0200

    Update

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+	//Require systeme if need login user
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="Content-Type" content="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+		<header>
+		
+		</header>
+		<main>
+			<section>
+				<form action="" method="POST">
+					<input id="username" type="text" placeholder="Username"/>
+					<input id="password" type="password" placeholder="Password"/>
+					<button id="login_btn" type="button">Login</button>
+					<button id="logout_btn" type="button">Logout</button>
+				</form>
+			</section>
+		</main>
+		<footer>
+		
+		</footer>
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
