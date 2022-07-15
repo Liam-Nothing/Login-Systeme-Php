8ee12728e2a02867bcdfef0105d8bedb1aaa69f5
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-07-15 23:26:44 +0200

    [Bugfix] API

--- api/login/getAuth.php
+++ api/login/getAuth.php
@@ -5,12 +5,13 @@
 $data = array(["username", 60, 1], ["password", 500, 1]);
 $data = data_security($data_from_client, $data);
 
-if (!($data["error"])) {
+if (empty($data["type"]) or $data["type"] != "error") {
 
 	// Test if user is in DB
 
 	$return_data["type"] = "success";
 	$return_data["message"] = "User connected";
+	$return_data["content"] = $data;
 } else {
 	$return_data["type"] = "error";
 	$return_data["message"] = "Client data error";
