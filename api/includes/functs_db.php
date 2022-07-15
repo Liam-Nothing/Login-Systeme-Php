a7d0130bd8ac913d5e5702e77527be4ba7b8eac8
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-07-15 22:56:45 +0200

    [Update] Indent

--- api/includes/functs_db.php
+++ api/includes/functs_db.php
@@ -1,18 +1,15 @@
 <?php
 
-    $config = json_decode(file_get_contents(dirname(__FILE__)."/config.json"), true);
+$config = [
+    "host" => "localhost",
+    "dbusername" => "root",
+    "dbpassword" => ""
+];
 
-    if(in_array($_SERVER['REMOTE_ADDR'], ["127.0.0.1"])){
-        $config = [
-            "host" => "localhost",
-            "dbusername" => "root",
-            "dbpassword" => ""
-        ];
-    }
-
-    function connectDB($dbname, $config) {
-        $pdo = new PDO("mysql:host=".$config["host"], $config["dbusername"], $config["dbpassword"]);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->query("use $dbname");
-        return $pdo;
-    }
+function connectDB($dbname, $config)
+{
+    $pdo = new PDO("mysql:host=" . $config["host"], $config["dbusername"], $config["dbpassword"]);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->query("use $dbname");
+    return $pdo;
+}
--- api/includes/selector_api.php
+++ api/includes/selector_api.php
@@ -1,23 +1,23 @@
 <?php
 
-    $data = array(["api", 60, 1]);
-    $data = data_security($data_from_client, $data);
+$data = array(["api", 60, 1]);
+$data = data_security($data_from_client, $data);
 
-    if(empty($data["error"])){
+if (empty($data["error"])) {
 
-        switch ($data["api"]) {
+    switch ($data["api"]) {
 
-			case "ne_app_manager_login":
-                require_once(dirname(__FILE__)."/../ne_app_manager/ne_app_manager_login.php");
-				break;
+        case "ne_app_manager_login":
+            require_once(dirname(__FILE__) . "/../ne_app_manager/ne_app_manager_login.php");
+            break;
 
-            default:
-                $return_data["id"] = 2;
-                $return_data["type"] = "error";
-                $return_data["message"] = "API doesnt exist";
-        }
-    }else{
-		$return_data["id"] = 2;
-		$return_data["type"] = "error";
-		$return_data["message"] = "Client data error";
-    }	
+        default:
+            $return_data["id"] = 2;
+            $return_data["type"] = "error";
+            $return_data["message"] = "API doesnt exist";
+    }
+} else {
+    $return_data["id"] = 2;
+    $return_data["type"] = "error";
+    $return_data["message"] = "Client data error";
+}
