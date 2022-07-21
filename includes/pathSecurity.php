773b8c3388f16dfaa0c2e1d572d290dac07705bc
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-07-21 09:22:52 +0200

    [WIP] Redirecting connection

--- includes/pathSecurity.php
+++ includes/pathSecurity.php
@@ -1,13 +1,12 @@
 <?php
 
 
-function ImConnected($needConnect = false, $redirectPageConnected = "index.php") {
+function ImConnected($Connected = false, $redirectPageConnected = "index.php") {
     // if(isset($_SESSION["id"])){
-    if($needConnect){
+    if($Connected){
         header('Location: '.$redirectPageConnected);
         exit(0);
     }else{
         $return_data["message"] = "You are not logged";
     }
 }
-
