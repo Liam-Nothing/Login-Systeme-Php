65aa0fab546fb7782b8c1ded6ccd9591751a4cc9
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-09-26 15:56:32 +0200

    [Update] Create board html

---
+++ js/scripts_board _php.js
@@ -0,0 +1,24 @@
+function AnswerAPI(answer_json, api_name) {
+    switch (api_name) {
+        case "logout":
+            console.log(answer_json);
+            window.location.replace("index.php");
+            break;
+
+        default:
+            console.log(`Api rep doesn't exist. ${JSON.parse(data)["api"]}.`);
+    }
+}
+
+function getLogout() {
+    let data = {};
+    data["api"] = "logout";
+
+    RequestAPI(url, JSON.stringify(data));
+}
+
+function Default() {
+    document.getElementById("btn_logout").addEventListener("click", function () { getLogout(); }, false);
+}
+
+Default();
--- js/scripts_board.js
+++ js/scripts_board.js
@@ -2,12 +2,30 @@ function AnswerAPI(answer_json, api_name) {
     switch (api_name) {
         case "logout":
             console.log(answer_json);
-            window.location.replace("index.php");
+            window.location.replace("index.html");
+            break;
+
+        case "getpermpage":
+            console.log(answer_json)
+            if (answer_json["type"] == "success" && answer_json["redirect"] != false) {
+                window.location.replace("http://" + window.location.hostname + "/" + answer_json["redirect"]);
+            }
             break;
 
         default:
             console.log(`Api rep doesn't exist. ${JSON.parse(data)["api"]}.`);
     }
+}
+
+function getPermPage() {
+    let data = {};
+    data["api"] = "getpermpage";
+    data["php_session_id"] = getPHPSessionId();
+    data["url"] = window.location.href;
+    data["redirect"] = "Login-Systeme-Php/index.html";
+    data["need_connect"] = 1;
+
+    RequestAPI(url, JSON.stringify(data));
 }
 
 function getLogout() {
@@ -22,3 +40,4 @@ function Default() {
 }
 
 Default();
+getPermPage();
